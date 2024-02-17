fbab9930c20081c05e4bb3ff0bcb869956e1444e
assaypoulose <94008297+assaypoulose@users.noreply.github.com>
2024-02-17 13:47:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let myResume={
+    "basics": {
+      "firstname": "Assay Poulose",
+      "lastname" : "Peenikkaparamban",
+      "email": "assaypoulose16@gamil.com",
+      "phone": +4917683466693,
+      "Batchelor degree": "B.E.Aeronautical Engineering",
+      "Master degree": "MSc.Aviation Transport",
+      "Certifications": "Full-Stack Development, Python, Javascript, SQL and Salesforce",
+      "location": {
+        "address": "Hüttenstraße 115",
+        "postalCode": 40227,
+        "city": "North Rhine-Westphalia",
+        "state": "Düsseldorf",
+        "country": "Germany",
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/assay-poulose-961911179/",
+          "github": "https://github.com/assaypoulose"
+        }
+      ]
+    },
+    "work": [
+        {
+          "company": "Innovative Travel Solutions",
+          "position": "Junior Project Manager",
+          "startDate": "06-06-2022",
+          "endDate": "31-01-2024",
+          "summary": "I play a key role as Junior Project Manager for Innovative Travel Solutions through Dyninno.Building Dashboard reports for giving updates on KPIs, sales reports on Month to Date and Month to Week basis. Solving issues related to CMS errors when tickets are raised. Providing Training Materials for sales floors. Managing the sales results and providing product updates. Involving with marketing team and keep track of leads generated through a pipeline system. Building rules for assigning request in incentive Que as well as auto assigned. Keeping track of approval requests and following up till final stage. Solving issues related to lead generation and product updates. Finding out areas that can be automated and passing the information to the development team. Involving in meetings.",
+        },
+      ],
+      "education": [
+        {
+          "institution": "Riga Technical University",
+          "department": "Aviation Transport",
+          "studyType": "fulltime",
+          "batch start year": 2018,
+          "batch end year": 2020,
+          "gpa": 6.5,
+        }
+      ],
+      "skills": [
+        {
+          "name": "python,javascript",
+          "level": "expert",
+        }
+          
+    ],
+        
+      "languages": [
+        {
+          "language": "Enlish,Hindi,Tamil,Malayalam,German",
+        }
+      ],
+      "interests": [
+        {
+          "name": "coding,Painting,Dancing,Reading",
+        }
+      ]
+    }
+    console.log(myResume);
